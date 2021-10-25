c61d893fd365a77f2529f38e290f17fa4444c9b1
subhomit <88931246+subhomit@users.noreply.github.com>
2021-10-25 21:53:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var colour = "red";
+ctx.beginPath();
+ctx.strokeStyle = colour;
+ctx.linewidth = 3;
+ctx.arc(200,200,40,0,2*Math.PI);
+canvas.stroke();
+canvas.addEventListener("mousedown",my_mousedown);
+function my_mousedown(e){
+    mouse_x = e.clientX-canvas.offsetLeft;
+    mouse_y = e.clientY-canvas.offsetTop;
+    console.log("X = " + mouse_x + " ,Y = " + mouse_y);
+    circle(mouse_x,mouse_y);
+}
+function circle(mouse_x,mouse_y){
+    ctx.beginPath();
+    ctx.strokeStyle = colour;
+    ctx.lineWidth = 3;
+    ctx.arc(mouse_x,mouse_y,40,0,2*Math.PI);
+    ctx.stroke();
+}
+function cleararea(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
